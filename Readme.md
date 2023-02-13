73e45c0eb5d93ee8ca08c35a8d45b0f8e71d5d1f
kimsoar <kimsoar17@gmail.com>
2023-02-13 19:52:36 +0900

    basic regex

---
+++ Readme.md
@@ -0,0 +1,5 @@
+Regular Expressions (RegEx) Tutorial
+
+https://www.youtube.com/watch?v=uaepGvA-iK4&list=PL4cUxeGkcC9g6m_6Sld9Q4jzqdqHd2HiD&index=2
+
+https://regex101.com/
--- validation.js
+++ validation.js
@@ -1,1 +1,30 @@
 // validation script here
+// var reg = /[a-z]/gi;
+// var reg2 = new RegExp(/[a-z]/, "ig");
+
+const inputs = document.querySelectorAll("input");
+
+const patterns = {
+  telephone: /^\d{11}$/,
+  username: /^[a-z\d]{5,12}$/,
+  password: /^[\w@-]{8,20}$/,
+  slug: /^[a-z\d-]{8,20}$/,
+  email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+};
+
+// validattion function
+
+function validate(field, regex) {
+  if (regex.test(field.value)) {
+    field.className = "valid";
+  } else {
+    field.className = "invalid";
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("keyup", (e) => {
+    //console.log(e.target.attributes.name.value);
+    validate(e.target, patterns[e.target.attributes.name.value]);
+  });
+});
